688d6888fcd0f85eefb946eee517b41ff403e472
Angelos Staboulis <angelosstaboulis@gmail.com>
2025-04-02 15:38:41 +0300

    Initial commit

---
+++ OSXDeleteDerivedData/OSXDeleteDerivedDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  OSXDeleteDerivedDataApp.swift
+//  OSXDeleteDerivedData
+//
+//  Created by Angelos Staboulis on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct OSXDeleteDerivedDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView().frame(width:970,height:400)
+        }.windowResizability(.contentSize)
+    }
+}
